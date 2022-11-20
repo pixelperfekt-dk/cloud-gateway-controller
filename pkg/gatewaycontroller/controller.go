218af41715b85b2d9ae7caf20eee45017849502c
MichaelVL <mvl.gh@network42.dk>
2022-11-20 20:45:24 +0100

    Add ownerrefs

--- pkg/gatewaycontroller/controller.go
+++ pkg/gatewaycontroller/controller.go
@@ -2,6 +2,7 @@ package gatewaycontroller
 
 import (
 	"context"
+	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -10,7 +11,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
+	gateway "sigs.k8s.io/gateway-api/apis/v1beta1"
 	//lister "sigs.k8s.io/gateway-api/pkg/client/listers/apis/v1beta1"
 )
 
@@ -23,17 +24,30 @@ type GatewayReconciler struct {
 //+kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=gateways/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=gateways/finalizers,verbs=update
 
+func (r *GatewayReconciler) constructGateway(gw_in *gateway.Gateway, class gateway.GatewayClass) (*gateway.Gateway, error) {
+	name := fmt.Sprintf("%s-%s", gw_in.ObjectMeta.Name, "istio") // FIXME create suffix from gatewayclass configmap
+	gw_out := gw_in.DeepCopy()
+	gw_out.ResourceVersion = ""
+	gw_out.ObjectMeta.Name = name
+	gw_out.Spec.GatewayClassName = "istio" // FIXME get from gatewayclass configmap
+
+	if err := ctrl.SetControllerReference(gw_in, gw_out, r.Scheme); err != nil {
+		return nil, err
+        }
+	return gw_out, nil
+}
+
 func (r *GatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
-	gw := &gatewayv1beta1.Gateway{}
+	gw := &gateway.Gateway{}
 	err := r.Get(ctx, req.NamespacedName, gw)
 	if err != nil {
 		return reconcile.Result{}, err
 	}
 	log.Info("reconsile", "gateway", gw)
 
-	classes := &gatewayv1beta1.GatewayClassList{}
+	classes := &gateway.GatewayClassList{}
 	err = r.List(ctx, classes, client.InNamespace(metav1.NamespaceAll))
 	if err != nil {
 		return reconcile.Result{}, err
@@ -42,11 +56,12 @@ func (r *GatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 
 	if gw.Spec.GatewayClassName == "default" {
 		log.Info("handled by us", "gatewayclass", gw.Spec.GatewayClassName)
-		gw_out := gw.DeepCopy()
-		gw_out.ResourceVersion = ""
-		gw_out.ObjectMeta.Name = gw_out.ObjectMeta.Name + "-istio"
-		gw_out.Spec.GatewayClassName = "istio"
 		// FIXME test not already existing
+		gw_out, err := r.constructGateway(gw, classes.Items[0]) // FIXME, find matching class
+		if err != nil {
+			log.Error(err, "unable to build Gateway object", "gateway", gw)
+			return ctrl.Result{}, err
+		}
 		if err := r.Create(ctx, gw_out); err != nil {
 			log.Error(err, "unable to create Gateway", "gateway", gw)
 			return ctrl.Result{}, err
@@ -58,6 +73,6 @@ func (r *GatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 
 func (r *GatewayReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&gatewayv1beta1.Gateway{}).
+		For(&gateway.Gateway{}).
 		Complete(r)
 }
